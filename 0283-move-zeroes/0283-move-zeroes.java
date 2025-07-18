96dbec28225d618e30d38dbaf59b4b73fddab86c
Yunus Emre İnci <151828173+yeinci@users.noreply.github.com>
2025-07-18 12:15:18 +0300

    Time: 1 ms (99.76%), Space: 46.2 MB (54.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n = nums.length, zeroCount = 0;
+        int n = nums.length, index = 0;
         ArrayList<Integer> numsArr = new ArrayList<>();
         for (int num : nums) {
-            if (num == 0) zeroCount++;
-            else numsArr.add(num);
+            if (num != 0) {
+                nums[index] = num;
+                index++;
+            }
         }
-        for (int i = 0; i < zeroCount; i++) {
-            numsArr.add(0);
-        }
-        for (int i = 0; i < n; i++) {
-            nums[i] = numsArr.get(i);
+        for (int i = index; i < n; i++) {
+            nums[i] = 0;
         }
     }
 }
